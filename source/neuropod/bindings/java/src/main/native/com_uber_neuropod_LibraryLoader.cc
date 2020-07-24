bedd731412230b1b99b120f5c3158c25f7d19b21
Weijia Deng <weijiad@uber.com>
2020-07-24 15:16:55 -0700

    Add bazel build file and load native library

--- source/neuropod/bindings/java/src/main/native/com_uber_neuropod_LibraryLoader.cc
+++ source/neuropod/bindings/java/src/main/native/com_uber_neuropod_LibraryLoader.cc
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Copyright (c) 2020 UATC, LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,8 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-=======
->>>>>>> fc2c4c8... Add bazel build file and load native library
 #include "com_uber_neuropod_LibraryLoader.h"
 
 #include "utils.h"
